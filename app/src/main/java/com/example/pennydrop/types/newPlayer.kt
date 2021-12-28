107bd677989c96cdcbff7edbd6ca3fd7b18c3c1b
OBigVee <adedejivictor98@gmail.com>
2021-12-28 23:17:31 +0100

    viewModel

--- app/src/main/java/com/example/pennydrop/types/newPlayer.kt
+++ app/src/main/java/com/example/pennydrop/types/newPlayer.kt
@@ -1,4 +1,14 @@
-package com.example.pennydrop.typess
+package com.example.pennydrop.types
 
-class newPlayer {
-}
+import android.widget.ToggleButton
+import androidx.databinding.ObservableBoolean
+
+data class NewPlayer (
+    var playerName : String = "",
+    val isHuman:ObservableBoolean = ObservableBoolean(true),
+    val canBeRemoved:Boolean = true,
+    val caBeToggled: Boolean = true,
+    var isIncluded: ObservableBoolean = ObservableBoolean(!canBeRemoved)
+
+)
+
--- app/src/main/java/com/example/pennydrop/viewmodels/PickPlayersViewModel.kt
+++ app/src/main/java/com/example/pennydrop/viewmodels/PickPlayersViewModel.kt
@@ -1,4 +1,22 @@
-package com.example.pennydrop.viewmodelss
+package com.example.pennydrop.viewmodels
 
-class PickPlayersViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.pennydrop.types.NewPlayer
+
+class PickPlayersViewModel:ViewModel() {
+    //players holds a list of new players
+    val players = MutableLiveData<List<NewPlayer>>().apply{
+        // this will be some players
+        // create a range of value from 1-6 and map the values
+        // each to a new instance
+        this.value = (1..6).map{
+            NewPlayer(
+                // conditional logic to parse logic to the pick player screen
+                canBeRemoved = it > 2, // last 4 players can be remove
+                caBeToggled = it > 1 // only last 5 players can be toggled
+            )
+        }
+    }
+
 }
