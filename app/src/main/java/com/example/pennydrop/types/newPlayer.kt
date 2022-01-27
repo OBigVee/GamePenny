9cd23f370145ebd3c254841ecc5ee81f71de1990
OBigVee <adedejivictor98@gmail.com>
2022-01-27 12:15:18 +0100

    feat
    a new function added to convert NewPlayer class into Player and get the players name or the AI name

--- app/src/main/java/com/example/pennydrop/types/newPlayer.kt
+++ app/src/main/java/com/example/pennydrop/types/newPlayer.kt
@@ -2,14 +2,32 @@ package com.example.pennydrop.types
 
 import android.widget.ToggleButton
 import androidx.databinding.ObservableBoolean
+import com.example.pennydrop.game.AI
 
 data class NewPlayer (
     var playerName : String = "",
     val isHuman:ObservableBoolean = ObservableBoolean(true),
     val canBeRemoved:Boolean = true,
     val canBeToggled: Boolean = true,
-    var isIncluded: ObservableBoolean = ObservableBoolean(!canBeRemoved),
-    var selectedAIPosition: Int = -1 // track the index
+    var isIncluded: ObservableBoolean = ObservableBoolean(false),
+    // track the index of selected AI(retrieved from Ai.BasicAI or AI.getBasicAI)
+    var selectedAIPosition: Int = -1
 
-)
+){
+    fun selectedAI() = if(!isHuman.get()){
+        AI.basicAI.getOrNull(selectedAIPosition)
+    }else{
+        null
+    }
+    fun toPlayer()=Player(
+        if(this.isHuman.get()){
+            this.playerName
+        }else{
+            (this.selectedAI()?.name?:"AI")
+        },
+        this.isHuman.get(),
+        this.selectedAI()
+    )
+}
+
 
