6dbbe7ed693054bbf8978d6f31beeb11ed871fce
OBigVee <adedejivictor98@gmail.com>
2022-02-19 16:10:46 +0100

    Feat: added an extension function clear to the Slot class the function clear can be used anywhere within the project pennyDrop.

--- app/src/main/java/com/example/pennydrop/types/Slot.kt
+++ app/src/main/java/com/example/pennydrop/types/Slot.kt
@@ -1,8 +1,27 @@
 package com.example.pennydrop.types
 
+/**
+ * Game slot properties which includes:
+ * the slot index-> number: Int,
+ * whether or not it can be filled -> canBeFilled:Boolean,
+ * is slot occupied? -> isFilled:Boolean,
+ * lastRolled
+ */
 data class Slot(
     val number:Int,
     val canBeFilled:Boolean = true,
     var isFilled:Boolean = false,
     var lastRolled:Boolean = false
 )
+
+/**
+ * clear function is an extension function added to the List type of Slot.kt file
+ * But the extension function is defined outside of Slot class.
+ * Since it is an extension on Slot class and not part of the Slot class itself,
+ * we can put the code anywhere that makes sense.
+ * I choose the Slot class and import it anywhere i want to use it.
+  */
+fun List<Slot>.clear() = this.forEach{ slot ->
+    slot.isFilled = false
+    slot.lastRolled = false
+}
